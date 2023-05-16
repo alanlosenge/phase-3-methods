f32d1b74f9c19fb4b4f398495b1263b07c2fd23c
Allan Losenge <temlosenge@gmail.com>
2023-05-16 11:02:06 +0300

    Done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def greet_programmer 
+    puts  "Hello, programmer!"
+ end  
+ greet_programmer
+ 
+ def greet(name)
+     puts "Hello, #{name}!"
+ end
+ greet("alan")
+ 
+  def greet_with_default(name)
+     puts "Hello, #{name}!"
+  end
+  greet_with_default("alan")
+ 
+  def greet_with_default(name="programmer")
+     puts "Hello, #{name}!"
+  end
+  greet_with_default
+ 
+  def add(a,b)
+     a+b
+   end
+ 
+ def halve(number)
+     number.to_i / 2 if number.to_i == number
+ end   
